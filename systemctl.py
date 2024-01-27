659690143833580c62fcde54a8656ed452dee5af
safarov.on <safarov.on@gmail.com>
2024-01-28 01:40:47 +0400

    .

--- systemctl.py
+++ systemctl.py
@@ -13,29 +13,32 @@ def main():
     user_ssh = "scuser"
     port_ssh = 22666
     # Get password securely
-    password_ssh = getpass.getpass("Enter SSH password: ")
-
-    # Establish SSH connection
-    con_ssh = establish_ssh_connection(hostname_ssh,user_ssh,port_ssh,password_ssh)
-
     try:
-        # Run a command on the remote server
-        result = con_ssh.run("hostnamectl", hide=True)
-        result1 = con_ssh.run("ls -la", hide=True)
-
-        print(result.stdout)
-        print(result1.stdout)
-
+        password_ssh = getpass.getpass("Enter SSH password: ")
     except KeyboardInterrupt:
         print("Session interrupted by you!")
-
-    except Exception:
-            # Catch other exceptions without printing details
-            pass
-
     finally:
-        # Close the SSH connection
-        con_ssh.close()
+        # Establish SSH connection
+        con_ssh = establish_ssh_connection(hostname_ssh,user_ssh,port_ssh,password_ssh)
+
+        try:
+            # Run a command on the remote server
+            result = con_ssh.run("hostnamectl", hide=True)
+            result1 = con_ssh.run("ls -la", hide=True)
+
+            print(result.stdout)
+            print(result1.stdout)
+
+        except KeyboardInterrupt:
+            print("Session interrupted by you!")
+
+        except Exception:
+                # Catch other exceptions without printing details
+                pass
+
+        finally:
+            # Close the SSH connection
+            con_ssh.close()
 
 if __name__ == "__main__":
     main()
