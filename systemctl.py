67dd8d098db03cd8c8fecb1847f6d16ddc23736a
safarov.on <safarov.on@gmail.com>
2024-01-28 00:58:38 +0400

    freshstart

--- systemctl.py
+++ systemctl.py
@@ -1,14 +1,39 @@
-import fabric
 import getpass
+from fabric import Connection, Config
 
-from fabric import Connection , Config
-hostnameSSH = "185.87.252.240"
-userSSH = "scuser"
-portSSH = 22666
+def establish_ssh_connection():
+    # Remote server details
+    hostname_ssh = "185.87.252.240"
+    user_ssh = "scuser"
+    port_ssh = 22666
 
-passwordSSH = getpass.getpass()
+    # Get password securely
+    password_ssh = getpass.getpass("Enter SSH password: ")
 
-config = Config(overrides={'sudo': {'password' : passwordSSH}})
-conSSH = Connection(hostnameSSH, user=userSSH, port=portSSH, config=config)
+    # Set up configuration with sudo password
+    config = Config(overrides={'sudo': {'password': password_ssh}})
 
-conSSH.run("hostnamectl")
+    # Establish SSH connection
+    connect_kwargs = {'password': password_ssh}
+    con_ssh = Connection(hostname_ssh, user=user_ssh, port=port_ssh, config=config, connect_kwargs=connect_kwargs)
+
+    return con_ssh
+
+def main():
+    # Establish SSH connection
+    con_ssh = establish_ssh_connection()
+
+    try:
+        # Run a command on the remote server
+        result = con_ssh.run("hostnamectl", hide=True)
+        
+        # Print the command output
+        print(result.stdout)
+
+    finally:
+        # Close the SSH connection
+        con_ssh.close()
+
+if __name__ == "__main__":
+    main()
+
