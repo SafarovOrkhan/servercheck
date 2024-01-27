881dcfea2d8d517b5f12aa18aa9d364b608e1aae
safarov.on <safarov.on@gmail.com>
2024-01-28 01:27:33 +0400

    .

--- systemctl.py
+++ systemctl.py
@@ -1,36 +1,34 @@
 import getpass
 from fabric import Connection, Config
 
-def establish_ssh_connection():
-    # Remote server details
-    hostname_ssh = "185.87.252.240"
-    user_ssh = "scuser"
-    port_ssh = 22666
-
-    # Get password securely
-    password_ssh = getpass.getpass("Enter SSH password: ")
-
-    # Set up configuration with sudo password
-    config = Config(overrides={'sudo': {'password': password_ssh}})
-
+def establish_ssh_connection(hostname_ssh,user_ssh,port_ssh,password_ssh):
     # Establish SSH connection
     connect_kwargs = {'password': password_ssh}
-    con_ssh = Connection(hostname_ssh, user=user_ssh, port=port_ssh, config=config, connect_kwargs=connect_kwargs)
-
+    con_ssh = Connection(hostname_ssh, user=user_ssh, port=port_ssh,connect_kwargs=connect_kwargs)
     return con_ssh
 
 def main():
+
+    hostname_ssh = "185.87.252.240"
+    user_ssh = "scuser"
+    port_ssh = 22666
+    # Get password securely
+    password_ssh = getpass.getpass("Enter SSH password: ")
+
     # Establish SSH connection
-    con_ssh = establish_ssh_connection()
+    con_ssh = establish_ssh_connection(hostname_ssh,user_ssh,port_ssh,password_ssh)
 
     try:
         # Run a command on the remote server
-        result = con_ssh.run("hostnamectl", hide=True)
-        result2 = con_ssh.run("ls -la", hide=True )
-        
-        # Print the command output
-        print(result.stdout)
-        print(result2.stdout)
+        #result = con_ssh.run("hostnamectl", hide=True)
+
+    except KeyboardInterrupt:
+        print("Session Interupted by you !")
+
+    except Exception:
+    # Catch other exceptions without printing details
+        pass
+
     finally:
         # Close the SSH connection
         con_ssh.close()
