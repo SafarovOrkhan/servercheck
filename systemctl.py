f61925a1a2cf236cb4f0b5e0ae4f697fa7c7d3ab
safarov.on <safarov.on@gmail.com>
2024-01-28 00:49:09 +0400

    test

--- systemctl.py
+++ systemctl.py
@@ -2,7 +2,13 @@ import fabric
 import getpass
 
 from fabric import Connection , Config
+hostnameSSH = "185.87.252.240"
+userSSH = "scuser"
+portSSH = 22666
 
-password = getpass.getpass()
+passwordSSH = getpass.getpass()
 
-print(password)
+config = Config(overrrides={'sudo': {'password' : passwordSSH}})
+conSSH = Connection(hostnameSSH, user=userSSH, port=portSSH, config=config)
+
+conSSH.run("hostnamectl")
