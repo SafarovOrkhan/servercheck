3c6975e130d7cd50f8fb7ed663143cb80ad3fdb6
safarov.on <safarov.on@gmail.com>
2024-01-28 02:03:19 +0400

    .

--- systemctl.py
+++ systemctl.py
@@ -1,43 +1,43 @@
 import getpass
 from fabric import Connection, Config
 
-def establish_ssh_connection(hostname_ssh,user_ssh,port_ssh,password_ssh):
+def establish_ssh_connection(hostname_ssh, user_ssh, port_ssh, password_ssh):
     # Establish SSH connection
     connect_kwargs = {'password': password_ssh}
-    con_ssh = Connection(hostname_ssh, user=user_ssh, port=port_ssh,connect_kwargs=connect_kwargs)
+    con_ssh = Connection(hostname_ssh, user=user_ssh, port=port_ssh, connect_kwargs=connect_kwargs)
     return con_ssh
 
 def main():
+    con_ssh = None
 
     hostname_ssh = "185.87.252.240"
     user_ssh = "scuser"
     port_ssh = 22666
-    # Get password securely
+
     try:
+        # Get password securely
         password_ssh = getpass.getpass("Enter SSH password: ")
-        con_ssh = establish_ssh_connection(hostname_ssh,user_ssh,port_ssh,password_ssh)
+
+        # Establish SSH connection
+        con_ssh = establish_ssh_connection(hostname_ssh, user_ssh, port_ssh, password_ssh)
+
+        # Run sudo commands on the remote server
+        result = con_ssh.sudo("hostnamectl", hide=True)
+        result1 = con_ssh.sudo("ls -la", hide=True)
+
+        print(result.stdout)
+        print(result1.stdout)
+
     except KeyboardInterrupt:
         print("Session interrupted by you!")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
     finally:
-        try:
-            # Run a command on the remote server
-            result = con_ssh.sudo("hostnamectl", hide=True)
-            result1 = con_ssh.sudo("ls -la", hide=True)
-
-            print(result.stdout)
-            print(result1.stdout)
-
-        except KeyboardInterrupt:
-            print("Session interrupted by you!")
-
-        except Exception:
-                # Catch other exceptions without printing details
-                pass
-
-        finally:
+        if con_ssh:
             # Close the SSH connection
             con_ssh.close()
 
 if __name__ == "__main__":
     main()
-
